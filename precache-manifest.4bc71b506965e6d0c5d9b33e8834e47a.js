c1044efee1f52c930a8ff2a6fb332ade0d42b082
JShaikh <jelonmusk@gmail.com>
2021-04-25 23:54:46 +0530

    Add files via upload

---
+++ precache-manifest.4bc71b506965e6d0c5d9b33e8834e47a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8a2374a42c60aa3b218a2244a8cf3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "586985ee7fc08b8aa955",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "6acda6edf814abb45735",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "95bce52212ac279490a8",
+    "url": "/static/css/main.09e59d6b.chunk.css"
+  },
+  {
+    "revision": "262d932456bbe38a4918",
+    "url": "/static/js/2.7344edd3.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.7344edd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586985ee7fc08b8aa955",
+    "url": "/static/js/3.c8485f16.chunk.js"
+  },
+  {
+    "revision": "6acda6edf814abb45735",
+    "url": "/static/js/4.cb9c067d.chunk.js"
+  },
+  {
+    "revision": "95bce52212ac279490a8",
+    "url": "/static/js/main.37ab84d1.chunk.js"
+  },
+  {
+    "revision": "8ec6b0a1280cd89935c7",
+    "url": "/static/js/runtime-main.f7283b71.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "7a1b7b252110aaed92360b5f46b0b457",
+    "url": "/static/media/dell.7a1b7b25.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "19635994a7ad68e56ddee9f50c3806c6",
+    "url": "/static/media/posiboltLogo.19635994.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "5fba008cf767a6c9cc197c420a05ab3c",
+    "url": "/static/media/turingLogo.5fba008c.png"
+  }
+]);
